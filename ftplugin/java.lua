95ddf7e14247f3a744640b3adc961fcfbd61567c
liguoyu <liguoyu@xiaomi.com>
2023-07-26 11:25:23 +0800

    feat: make jdtls working, exception for java 1.8

---
+++ ftplugin/java.lua
@@ -0,0 +1,237 @@
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.cmdheight = 2 -- more space in the neovim command line for displaying messages
+
+local jdtls = require('jdtls')
+
+local home = os.getenv "HOME"
+
+  -- 💀
+  -- This is the default if not provided, you can remove it. Or adjust as needed.
+  -- One dedicated LSP server & client will be started per unique root_dir
+local root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'})
+
+-- The nvim-cmp supports additional LSP's capabilities so we need to
+-- advertise it to LSP servers..
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+  -- eclipse.jdt.ls stores project specific data within a folder. If you are working
+  -- with multiple different projects, each project must use a dedicated data directory.
+  -- This variable is used to configure eclipse to use the directory name of the
+  -- current project found using the root_marker as the folder for project specific data.
+local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+
+-- Helper function for creating keymaps
+function nnoremap(rhs, lhs, bufopts, desc)
+  bufopts.desc = desc
+  vim.keymap.set("n", rhs, lhs, bufopts)
+end
+
+-- The on_attach function is used to set key maps after the language server
+-- attaches to the current buffer
+local on_attach = function(client, bufnr)
+  jdtls.setup_dap({ hotcodereplace = 'auto' })
+  jdtls.setup.add_commands()
+
+  -- Regular Neovim LSP client keymappings
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  nnoremap('gD', vim.lsp.buf.declaration, bufopts, "Go to declaration")
+  nnoremap('gd', vim.lsp.buf.definition, bufopts, "Go to definition")
+  nnoremap('gi', vim.lsp.buf.implementation, bufopts, "Go to implementation")
+  nnoremap('K', vim.lsp.buf.hover, bufopts, "Hover text")
+  nnoremap('<C-k>', vim.lsp.buf.signature_help, bufopts, "Show signature")
+  nnoremap('<space>wa', vim.lsp.buf.add_workspace_folder, bufopts, "Add workspace folder")
+  nnoremap('<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts, "Remove workspace folder")
+  nnoremap('<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts, "List workspace folders")
+  nnoremap('<space>D', vim.lsp.buf.type_definition, bufopts, "Go to type definition")
+  nnoremap('<space>rn', vim.lsp.buf.rename, bufopts, "Rename")
+  nnoremap('<space>ca', vim.lsp.buf.code_action, bufopts, "Code actions")
+  vim.keymap.set('v', "<space>ca", "<ESC><CMD>lua vim.lsp.buf.range_code_action()<CR>",
+    { noremap=true, silent=true, buffer=bufnr, desc = "Code actions" })
+  nnoremap('<space>f', function() vim.lsp.buf.format { async = true } end, bufopts, "Format file")
+
+  -- Java extensions provided by jdtls
+  nnoremap("<C-o>", jdtls.organize_imports, bufopts, "Organize imports")
+  nnoremap("<space>ev", jdtls.extract_variable, bufopts, "Extract variable")
+  nnoremap("<space>ec", jdtls.extract_constant, bufopts, "Extract constant")
+  vim.keymap.set('v', "<space>em", [[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]],
+    { noremap=true, silent=true, buffer=bufnr, desc = "Extract method" })
+
+    -- Java extensions
+  nnoremap("<C-o>", jdtls.organize_imports, bufopts, "Organize imports")
+  nnoremap("<leader>vc", jdtls.test_class, bufopts, "Test class (DAP)")
+  nnoremap("<leader>vm", jdtls.test_nearest_method, bufopts, "Test method (DAP)")
+  nnoremap("<space>ev", jdtls.extract_variable, bufopts, "Extract variable")
+  nnoremap("<space>ec", jdtls.extract_constant, bufopts, "Extract constant")
+  nnoremap("<space>em", [[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]], bufopts, "Extract method")
+end
+
+
+-- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+local config = {
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  cmd = {
+
+    -- use java 17
+    '/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home/bin/java',
+
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+    '-javaagent:' .. home .. '/.local/share/lombok/lombok-1.18.22.jar',
+
+    -- 💀
+    '-jar', '/opt/homebrew/Cellar/jdtls/1.25.0/libexec/plugins/org.eclipse.equinox.launcher_1.6.500.v20230622-2056.jar',
+         -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+         -- Must point to the                                                     Change this to
+         -- eclipse.jdt.ls installation                                           the actual version
+
+
+    -- 💀
+    '-configuration', '/opt/homebrew/Cellar/jdtls/1.25.0/libexec/config_mac/',
+                    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+                    -- Must point to the                      Change to one of `linux`, `win` or `mac`
+                    -- eclipse.jdt.ls installation            Depending on your system.
+
+
+    -- 💀
+    -- See `data directory configuration` section in the README
+    '-data', workspace_folder 
+  },
+
+  -- 💀
+  -- This is the default if not provided, you can remove it. Or adjust as needed.
+  -- One dedicated LSP server & client will be started per unique root_dir
+  root_dir = root_dir,
+  on_attach = on_attach,
+  capabilities = capabilities,
+
+
+  -- Here you can configure eclipse.jdt.ls specific settings
+  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+  -- for a list of options
+  settings = {
+    java = {
+      format = {
+        settings = {
+          -- Use Google Java style guidelines for formatting
+          -- To use, make sure to download the file from https://github.com/google/styleguide/blob/gh-pages/eclipse-java-google-style.xml
+          -- and place it in the ~/.local/share/eclipse directory
+          url = home .. "/.local/share/eclipse/eclipse-java-google-style.xml",
+          profile = "GoogleStyle",
+        },
+      },
+      signatureHelp = { enabled = true },
+      contentProvider = { preferred = 'fernflower' },
+      completion = {
+        favoriteStaticMembers = {
+          "org.hamcrest.MatcherAssert.assertThat",
+          "org.hamcrest.Matchers.*",
+          "org.hamcrest.CoreMatchers.*",
+          "org.junit.jupiter.api.Assertions.*",
+          "java.util.Objects.requireNonNull",
+          "java.util.Objects.requireNonNullElse",
+          "org.mockito.Mockito.*"
+        },
+        filteredTypes = {
+          "com.sun.*",
+          "io.micrometer.shaded.*",
+          "java.awt.*",
+          "jdk.*",
+          "sun.*",
+        },
+      },
+      sources = {
+        organizeImports = {
+          starThreshold = 9999;
+          staticStarThreshold = 9999;
+        },
+      },
+      codeGeneration = {
+        toString = {
+          template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
+        },
+        hashCodeEquals = {
+          useJava7Objects = true,
+        },
+        useBlocks = true,
+      },
+      configuration = {
+        runtimes = {
+          {
+            name = "JavaSE-1.8",
+            path = "/Library/Java/JavaVirtualMachines/zulu-8.jdk/Contents/Home/",
+          },
+--          {
+--            name = "JavaSE-11",
+--            path = "/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home/",
+--          },
+--          {
+--            name = "JavaSE-17",
+--            path = home .. "/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home/",
+--          },
+        }
+      },
+    },
+  },
+
+  flags = {
+    allow_incremental_sync = true,
+    debounce_text_changes = 80,
+  },
+
+  -- Language server `initializationOptions`
+  -- You need to extend the `bundles` with paths to jar files
+  -- if you want to use additional eclipse.jdt.ls plugins.
+  --
+  -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+  --
+  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+  init_options = {
+    bundles = {},
+  },
+}
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  pattern = { "*.java" },
+  callback = function()
+    vim.lsp.codelens.refresh()
+  end,
+})
+
+-- This starts a new client & server,
+-- or attaches to an existing client & server depending on the `root_dir`.
+-- jdtls.start_or_attach(config)
+require('jdtls').start_or_attach(config)
+
+vim.cmd "command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>)"
+vim.cmd "command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_set_runtime JdtSetRuntime lua require('jdtls').set_runtime(<f-args>)"
+vim.cmd "command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()"
+vim.cmd "command! -buffer JdtBytecode lua require('jdtls').javap()"
+
+-- Shorten function name
+local keymap = vim.keymap.set
+-- Silent keymap option
+local opts = { silent = true }
+
+keymap("n", "<leader>jo", "<Cmd>lua require'jdtls'.organize_imports()<CR>", opts)
+keymap("n", "<leader>jv", "<Cmd>lua require('jdtls').extract_variable()<CR>", opts)
+keymap("n", "<leader>jc", "<Cmd>lua require('jdtls').extract_constant()<CR>", opts)
+keymap("n", "<leader>jt", "<Cmd>lua require'jdtls'.test_nearest_method()<CR>", opts)
+keymap("n", "<leader>jT", "<Cmd>lua require'jdtls'.test_class()<CR>", opts)
+keymap("n", "<leader>ju", "<Cmd>JdtUpdateConfig<CR>", opts)
+
+keymap("v", "<leader>jv", "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", opts)
+keymap("v", "<leader>jc", "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", opts)
+keymap("v", "<leader>jm", "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", opts)
---
+++ lua/user/nvim-jdtls.lua
@@ -0,0 +1,22 @@
+local M = {
+  "mfussenegger/nvim-jdtls",
+  --event = "VeryLazy",
+  --commit = "3a148dac526396678f141a033270961d0d9ccb88"
+}
+
+function M.config()
+--   require("nvim-jdtls").setup {
+--     override = {
+--       zsh = {
+--         icon = "",
+--         color = "#428850",
+--         cterm_color = "65",
+--         name = "Zsh",
+--       },
+--     },
+--     color_icons = true,
+ --    default = true,
+--   }
+end
+
+return M
